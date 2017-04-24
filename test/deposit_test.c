71ab6e7362234fb6246393654fd1718bd2e99f99
AnnaVinnik <is641s22@lwp05.csc.local>
2017-04-24 09:11:54 +0700

    New test

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -34,87 +34,87 @@ CTEST(Input_data, test6)
 }
 
 
-CTEST(getsum, test1)
-{
-	ASSERT_EQUAL(9000, itog(1, 10001));
-}
-
-CTEST(getsum, test2)
-{
-	ASSERT_EQUAL(-1, itog(0, 10001));
-}
-
-CTEST(getsum, test3)
-{
-	ASSERT_EQUAL(-1, itog(1, 9999));
-}
-
-CTEST(getsum, test4)
-{
-	ASSERT_EQUAL(103000, itog(100, 100001));
-}
-
-CTEST(getsum, test5)
-{
-	ASSERT_EQUAL(-1, itog(100, 9999));
-}
-
-CTEST(getsum, test6)
-{
-	ASSERT_EQUAL(11200, itog(365, 10001));
-}
-
 CTEST(getsum, test7)
 {
-	ASSERT_EQUAL(103000, itog(31, 100001));
+	ASSERT_EQUAL(9000, getsum(1, 10000));
 }
 
 CTEST(getsum, test8)
 {
-	ASSERT_EQUAL(81000, itog(30, 100001));
+	ASSERT_EQUAL(9000, getsum(0, 10000));
 }
 
 CTEST(getsum, test9)
 {
-	ASSERT_EQUAL(108000, itog(240, 100001));
+	ASSERT_EQUAL(9000, getsum(1, 10000));
 }
 
 CTEST(getsum, test10)
 {
-	ASSERT_EQUAL(115000, itog(241, 100001));
+	ASSERT_EQUAL(10200, getsum(100, 10000));
 }
 
 CTEST(getsum, test11)
 {
-	ASSERT_EQUAL(10200, itog(31, 10001));
+	ASSERT_EQUAL(10200, getsum(100, 10000));
 }
 
 CTEST(getsum, test12)
 {
-	ASSERT_EQUAL(10600, itog(121, 10001));
+	ASSERT_EQUAL(11200, getsum(365, 10000));
 }
 
 CTEST(getsum, test13)
 {
-	ASSERT_EQUAL(10200, itog(120, 10001));
+	ASSERT_EQUAL(10200, getsum(31, 10000));
 }
 
 CTEST(getsum, test14)
 {
-	ASSERT_EQUAL(10600, itog(240, 10001));
+	ASSERT_EQUAL(9000, getsum(30, 10000));
 }
 
 CTEST(getsum, test15)
 {
-	ASSERT_EQUAL(11200, itog(241, 10001));
+	ASSERT_EQUAL(10600, getsum(240, 10000));
 }
 
 CTEST(getsum, test16)
 {
-	ASSERT_EQUAL(11200, itog(365, 10001));
+	ASSERT_EQUAL(11200, getsum(241, 10000));
 }
 
 CTEST(getsum, test17)
 {
-	ASSERT_EQUAL(-1, itog(366, 10001));
+	ASSERT_EQUAL(10200, getsum(31, 10000));
+}
+
+CTEST(getsum, test18)
+{
+	ASSERT_EQUAL(10600, getsum(121, 10000));
+}
+
+CTEST(getsum, test19)
+{
+	ASSERT_EQUAL(10200, getsum(120, 10000));
+}
+
+CTEST(getsum, test20)
+{
+	ASSERT_EQUAL(10600, getsum(240, 10000));
+}
+
+CTEST(getsum, test21)
+{
+	ASSERT_EQUAL(11200, getsum(241, 10000));
+}
+
+CTEST(getsum, test22)
+{
+	ASSERT_EQUAL(11200, getsum(365, 10000));
+}
+
+CTEST(getsum, test23)
+{
+	ASSERT_EQUAL(10000, getsum(366, 10000));
 }
